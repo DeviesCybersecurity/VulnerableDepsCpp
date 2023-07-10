5c6b07e06830e8636f46482a81e188a0563cf4c1
Andreas Lindmark <andreas.lindmark@devies.se>
2023-07-10 23:01:00 +0200

    Added nhlomman dependency and implemented a simple read config,

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+//https://medium.com/@dexterchan_44737/visual-studio-code-build-and-debug-a-c-with-cmake-on-mac-os-7633bc59bf34
+//https://code.visualstudio.com/docs/cpp/CMake-linux
+int main(void) {
+    std::cout << "hello world!";
+    return 0;
+}
