82b5c5631fc8b29cec044ab6728db67f924f46d3
Andreas Lindmark <andreas.lindmark@devies.se>
2023-07-10 23:07:46 +0200

    Previos commit did not include the Nhlomman dependencies correctly

--- main.cpp
+++ main.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <nlohmann/json.hpp>
+
 //https://medium.com/@dexterchan_44737/visual-studio-code-build-and-debug-a-c-with-cmake-on-mac-os-7633bc59bf34
 //https://code.visualstudio.com/docs/cpp/CMake-linux
+
+//https://github.com/nlohmann/json
+//https://github.com/eclipse/mosquitto
+//https://github.com/iDoka/awesome-canbus
+
+
+//SBOM in ubuntu & debian :  https://discourse.ubuntu.com/t/creating-a-software-bill-of-materials-sbom/25519
+
+using json = nlohmann::json;
+
 int main(void) {
-    std::cout << "hello world!";
+    std::cout << "Reading config.\n";
+    try {
+        std::ifstream f("config.json");
+        json data = json::parse(f);
+
+        for (auto& [key, value] : data.items()) {
+            std::cout << key << " : " << value << "\n";
+        }
+
+    }
+    catch(const std::ifstream::failure& e) {
+        std::cout << "Error " << e.what();
+    }
+    catch(const nlohmann::detail::parse_error &pe){
+        std::cout << "Error " << pe.what();
+    }
     return 0;
 }
